b89caae9921ab30ce9f7b6e24b8ac8fbe3191879
Julie <juliet@ksu.edu>
2025-08-26 16:24:00 -0500

    notes shell before lab2

---
+++ monWed_sectionA/week1_aug25-29/wedAug27/Pizza.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Pizza {
+    public static void main(String[] args) {
+        /*
+        Example: pizzas have 8 slices. Want enough pizzas 
+        that everyone can have at least two slices but with 
+        the minimum leftovers.
+
+        Get #people, print needed #pizzas. 
+        Say how many leftover slices.
+        */
+
+        Scanner s = new Scanner(System.in);
+        
+    }
+}
