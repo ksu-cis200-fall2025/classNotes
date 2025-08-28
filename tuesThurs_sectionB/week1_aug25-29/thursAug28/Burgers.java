0c1f0c3d5ccc5e62942c076cb3d245db5dcce2d6
Julie <juliet@ksu.edu>
2025-08-28 13:49:49 -0500

    after thurs aug 28

--- tuesThurs_sectionB/week1_aug25-29/thursAug28/Burgers.java
+++ tuesThurs_sectionB/week1_aug25-29/thursAug28/Burgers.java
@@ -1,6 +1,18 @@
+import java.util.*;
+
 public class Burgers {
     public static void main(String[] args) {
-        //burgers are $6.79 each. Get number of burgers, print total.
+        //burgers are $6.79 each. 
+        //Get number of burgers, print total.
 
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter number of burgers: ");
+        int burgers = s.nextInt();
+
+        double total = burgers*6.79;
+
+        //Your total is: $x.xx
+        System.out.printf("Your total is: $%.2f%n", total);
     }
 }
--- tuesThurs_sectionB/week1_aug25-29/thursAug28/Pizza.java
+++ tuesThurs_sectionB/week1_aug25-29/thursAug28/Pizza.java
@@ -3,12 +3,25 @@ import java.util.*;
 public class Pizza {
     public static void main(String[] args) {
         /*
-        Example: pizzas have 8 slices. Get #pizzas, #people. 
+        Example: pizzas have 8 slices. 
+        Get #pizzas, #people. 
 
         Print how many slices everyone can have, how many are leftover.
         */
 
         Scanner s = new Scanner(System.in);
         
+        System.out.print("Enter number of pizzas: ");
+        int pizzas = s.nextInt();
+
+        System.out.print("Enter number of people: ");
+        int people = s.nextInt();
+
+        int totalSlices = pizzas * 8;
+        int serving = totalSlices / people;
+        int leftover = totalSlices % people;
+
+        //Everyone% gets 4 slices with 2 leftover
+        System.out.printf("Everyone gets %d slices with %d leftover%n" , serving, leftover); 
     }
 }
