a6f5e4d1cdd12544b377735dcc501643784734bd
Julie <juliet@ksu.edu>
2025-09-05 14:58:22 -0500

    added notes for lab 4

---
+++ monWed_sectionA/week3_sep8-12/monSep8/Factorial.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter a postive number: ");
+        int num = s.nextInt();
+
+        //print the factorial of num
+        //how to add input validation?
+    }
+}
