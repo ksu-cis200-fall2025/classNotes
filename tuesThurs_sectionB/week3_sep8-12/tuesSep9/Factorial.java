990c5c113d5461ecbf880f71ea5c6177beb230f8
Julie <juliet@ksu.edu>
2025-09-09 14:01:26 -0500

    after tues sep 9

--- tuesThurs_sectionB/week3_sep8-12/tuesSep9/Factorial.java
+++ tuesThurs_sectionB/week3_sep8-12/tuesSep9/Factorial.java
@@ -7,7 +7,23 @@ public class Factorial {
         System.out.print("Enter a postive number: ");
         int num = s.nextInt();
 
-        //print the factorial of num
+        if (num >= 1) {
+            //5! = 5*4*3*2*1
+            int result = 1; //multiplied so far
+
+            for (int i = num; i >= 1; i--) {
+                result *= i;
+            }
+
+            //print the factorial of num
+            System.out.printf("%d! = %d%n", num, result);
+        }
+        else {
+            System.out.printf("%d! is undefined%n", num);
+        }
+        
+
+        //5! = 120
         //how to add input validation?
     }
 }
