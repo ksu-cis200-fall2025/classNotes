36770bf0741fee3fd942ebe496d00ff98378ccc8
Julie <juliet@ksu.edu>
2025-09-10 14:18:23 -0500

    after wed sep 10

--- monWed_sectionA/week3_sep8-12/wedSep10/DiceGame.java
+++ monWed_sectionA/week3_sep8-12/wedSep10/DiceGame.java
@@ -3,18 +3,53 @@ import java.util.*;
 public class DiceGame {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        Random r = new Random();
 
-        //game goal: get to at least 20 points (higher is better)
+        char again;
+        int total = 0;
+        do {
+            //roll a dice
+            int roll = r.nextInt(6) + 1;
+            System.out.printf("You rolled %d%n", roll);
+
+            //did they roll a 1?
+            if (roll == 1) {
+                total = 0;
+                System.out.println("Total is zeroed, game over.");
+                break;
+            }
+            else {
+                total = total + roll;
+            }
+            
+            System.out.printf("Your current total is %d%n", total);
+
+            //add it to total, update stuff
+            //do they want to go again?
+            System.out.print("Do you want to roll again? (y/n) ");
+            again = (s.nextLine().toLowerCase()).charAt(0);
+
+        } while (again == 'y');
+
+        //print end of game stuff
+        if (total >= 20) {
+            System.out.printf("Total: %d points. You win!%n", total);
+        }
+        else {
+            System.out.printf("Total: %d points. You lose.%n", total);
+        }
+
+        //game goal: get to at least 20 points 
+        // (higher is better)
 
         //user starts with 0 points
-        //each time, user can choose to stop or roll (a 6-sided die)
+        //each time, user can choose to stop or roll 
+        // (a 6-sided die)
 
         //game ends when either: they roll a 1, or
             //they choose to stop
 
-        //if they roll a 1: game over, score = 0, they lose
-
-        //if they stop with at least 20 points, they win
+        //if they roll a 1: game over, score = 0
 
         //when game is over, print win/lose and score
     }
