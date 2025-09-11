d2f8e3cd77cfcf2178fabd9441339ddbe0e7dd63
Julie <juliet@ksu.edu>
2025-09-11 13:54:48 -0500

    after thurs sep 11

--- tuesThurs_sectionB/week3_sep8-12/thursSep11/DiceGame.java
+++ tuesThurs_sectionB/week3_sep8-12/thursSep11/DiceGame.java
@@ -4,6 +4,45 @@ public class DiceGame {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
+        Random r = new Random();
+
+        //first pass: repeatedly "roll" a dice and print
+        //ask user if they want to go again
+
+        char again;
+        int score = 0;
+        do {
+            int roll = r.nextInt(6) + 1;
+            System.out.printf("You rolled %d%n", roll);
+
+            if (roll == 1) {
+                score = 0;
+                System.out.println("Your total is zeroed");
+                break;
+            }
+            else {
+                score += roll;
+            
+                System.out.printf("Total score is: %d%n", score);
+                System.out.println();
+
+                System.out.print("Do you want to go again? (y/n) ");
+                again = s.nextLine().charAt(0);
+            }
+            
+        } while (again == 'y');
+
+        //print win/lose, final score
+        System.out.printf("Your final score is: %d%n", score);
+        if (score >= 20) {
+            System.out.println("You win!");
+        }
+        else {
+            System.out.println("You lose.");
+        }
+
+
+
         //game goal: get to at least 20 points (higher is better)
 
         //user starts with 0 points
