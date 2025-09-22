6798ce2ebd17f47f20ac134aa97bfddecf994e05
Julie <juliet@ksu.edu>
2025-09-22 14:11:00 -0500

    after mon sep 22

--- monWed_sectionA/week5_sep22-26/monSep22/Sep22.java
+++ monWed_sectionA/week5_sep22-26/monSep22/Sep22.java
@@ -3,5 +3,28 @@ import java.util.*;
 public class Sep22 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
+        /*
+            Get 6 numbers from the user.
+
+            Print whether any input number is the square of another 
+            input number.
+        */
+
+        int[] nums = new int[6];
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print("Enter number: ");
+            nums[i] = s.nextInt();
+        }
+
+        //nums: {0,3,2,1,4,9}
+        for (int i = 0; i < nums.length; i++) {
+            //seeing if the square of nums[i] is in there
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i]*nums[i] == nums[j] && (i != j)) {
+                    System.out.printf("%d^2 = %d%n", nums[i], nums[j]);
+                }
+            }
+        }
     }
 }
