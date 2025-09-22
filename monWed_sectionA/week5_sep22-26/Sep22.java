edc09016c4267cff8db332917264147d79bc913a
Julie <juliet@ksu.edu>
2025-09-22 11:03:31 -0500

    before first lab week sep 22

---
+++ monWed_sectionA/week5_sep22-26/Sep22.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Sep22 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+    }
+}
---
+++ tuesThurs_sectionB/week5_sep22-26/tuesSep23/Sep23.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Sep23 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+    }
+}
