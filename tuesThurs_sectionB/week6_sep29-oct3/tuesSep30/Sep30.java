fc956c9548480916db98273f5e35eaf1ce1c2c13
Julie <juliet@ksu.edu>
2025-09-26 13:50:13 -0500

    added notes before lab 9

---
+++ tuesThurs_sectionB/week6_sep29-oct3/tuesSep30/Sep30.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Sep30 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        //ask user for name of file
+        
+        //create output file named copy+origName
+        //that is a duplicate of original file
+
+    }
+}
