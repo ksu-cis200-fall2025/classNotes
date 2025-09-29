407f4014fac008769a89b7ecc26ad322707a72f5
Julie <juliet@ksu.edu>
2025-09-29 14:05:57 -0500

    after mon sep 29

--- monWed_sectionA/week6_sep29-oct3/monSep29/Sep29.java
+++ monWed_sectionA/week6_sep29-oct3/monSep29/Sep29.java
@@ -1,11 +1,29 @@
 import java.util.*;
+import java.io.*;
 
 public class Sep29 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
 
         //ask user for name of file
+        System.out.print("Enter name of file: ");
+        String filename = s.nextLine();
+
+        //connect to input file
+        Scanner inFile = new Scanner(new File(filename));
+
+        //connect to output file
+        PrintWriter outFile = new PrintWriter("copy_"+filename);
         
+        //read all lines in input file
+        while (inFile.hasNext()) {
+            String line = inFile.nextLine();
+            outFile.println(line);
+        }
+
+        inFile.close();
+        outFile.close();
+
         //create output file named copy+origName
         //that is a duplicate of original file
 
