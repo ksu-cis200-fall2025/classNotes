f6e8facb964d675f15517aeafc0e5037bedee7c5
Julie <juliet@ksu.edu>
2025-10-01 11:16:41 -0500

    notes before oct 2 and 3

---
+++ tuesThurs_sectionB/week6_sep29-oct3/thursOct2/Oct2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Oct2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter a list of numbers separated by commas: ");
+        String line = s.nextLine();
+
+        //like: 12,4,-3.2,5
+
+        //how to print the sum of the numbers WITHOUT split?
+    }
+}
