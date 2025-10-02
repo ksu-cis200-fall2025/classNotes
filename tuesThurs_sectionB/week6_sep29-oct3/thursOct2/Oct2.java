7aeea3215b41953407eb01f7205098baa7086717
Julie <juliet@ksu.edu>
2025-10-02 13:50:16 -0500

    after thurs oct 2

--- tuesThurs_sectionB/week6_sep29-oct3/thursOct2/Oct2.java
+++ tuesThurs_sectionB/week6_sep29-oct3/thursOct2/Oct2.java
@@ -10,5 +10,26 @@ public class Oct2 {
         //like: 12,4,-3.2,5
 
         //how to print the sum of the numbers WITHOUT split?
+
+        double sum = 0;
+        int index = 0;
+        boolean done = false;
+        do {
+            int comma = line.indexOf(",", index);
+
+            String piece;
+            if (comma >= 0) {
+                piece = line.substring(index,comma);
+            }
+            else {
+                piece = line.substring(index);
+                done = true;
+            }
+            sum += Double.parseDouble(piece);
+            
+            index = comma + 1;
+        } while (!done);
+
+        System.out.printf("Sum: %.2f%n", sum);
     }
 }
