218444602ee3cca42cb9f623d1c1f3f9ac926f8e
Julie <juliet@ksu.edu>
2025-10-03 14:41:51 -0500

    notes shell before mon oct 6

---
+++ tuesThurs_sectionB/week7_oct6-10/tuesOct7/Oct7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Oct7 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+    }
+
+    //write a method to compute the absolute value
+
+    //write a method to return whether a String 
+    //is a palindrome
+
+    //write a method to return position of 
+    //an element in an array
+    //what if we knew the array was sorted?
+}
