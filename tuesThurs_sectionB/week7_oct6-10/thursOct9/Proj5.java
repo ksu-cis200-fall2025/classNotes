858f44f1667748ef830a206cceb3b81b3aa06b93
Julie <juliet@ksu.edu>
2025-10-09 13:40:54 -0500

    after thurs oct 9

--- tuesThurs_sectionB/week7_oct6-10/thursOct9/Proj5.java
+++ tuesThurs_sectionB/week7_oct6-10/thursOct9/Proj5.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.io.*;
 
 public class Proj5 {
-    public static void main(String[] args) throws IOException {
+    public static void printGameInfo() {
         System.out.println("Welcome to text-based Wordle! You get 6 tries to guess a five-letter word.");
         System.out.println("Each guess must be a valid English word.");
         System.out.println("After each guess, you will get feedback for each letter in your guess.");
@@ -12,22 +12,91 @@ public class Proj5 {
         System.out.println("\"Y\" means that letter appears at a different position in the correct word.");
         System.out.println("\"B\" means that letter does not appear in the correct word.");
         System.out.println();
+    }
 
+    public static String[] readWords(String filename) throws IOException {
+        Scanner inFile = new Scanner(new File(filename));
+
+        String[] words = new String[Integer.parseInt(inFile.nextLine())];
+        for (int i = 0; i < words.length; i++) {
+            words[i] = inFile.nextLine();
+        }
+        inFile.close();
+
+        return words;
+    }
+
+    public static String buildSpacedWord(String guess) {
+        StringBuilder build = new StringBuilder();
+        for (int i = 0; i < guess.length(); i++) {
+            build.append(guess.charAt(i) + " ");
+        }
+        return build.toString();
+    }
+
+    public static boolean isValid(String guess, String[] words1, String[] words2) {
+        boolean found = false;
+        for (int i = 0 ; i < words1.length; i++) {
+            if (guess.equals(words1[i])) found = true;
+        }
+        for (int i = 0 ; i < words2.length; i++) {
+            if (guess.equals(words2[i])) found = true;
+        } 
+
+        return found;
+    } 
+
+    public static char[] buildFeedback(String guess, String correct) {
+        //build feedback
+        //G = correct letter, correct spot
+        //Y = correct letter, wrong spot
+        //B = wrong letter 
+
+        char[] feedback = new char[guess.length()];
+        boolean[] used = new boolean[guess.length()];
+
+        //find the correct letter/correct spot (G), mark that spot as used
+        for (int i = 0; i < guess.length(); i++) {
+            if (guess.charAt(i) == correct.charAt(i)) {
+                feedback[i] = 'G';
+                used[i] = true;
+            }
+        }
+
+        //now go back and find the correct letters/wrong spots (Y)
+        //if guess has x occurrences of the letter and correct has y occurrences, and x > y, only
+        //the first y occurrences in guess should get marked as Y (or G)
+        for (int i = 0; i < guess.length(); i++) {
+            if (feedback[i] == 'G') continue;
+
+            boolean matched = false;
+            for (int j = 0; j < correct.length(); j++) {
+                if (guess.charAt(i) == correct.charAt(j) && !used[j]) {
+                    //only possible if i != j
+
+                    matched = true;
+
+                    //prevents us from matching the same correct letter against a different (duplicate)
+                    //letter in guess
+                    used[j] = true;
+                    break;
+                }
+            }
+
+            if (matched) feedback[i] = 'Y';
+            else feedback[i] = 'B';
+        }
+
+        return feedback;
+    }
+
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
-        Scanner inAllowed = new Scanner(new File("allowed.txt"));
 
-        String[] allowed = new String[Integer.parseInt(inAllowed.nextLine())];
-        for (int i = 0; i < allowed.length; i++) {
-            allowed[i] = inAllowed.nextLine();
-        }
-        inAllowed.close();
+        printGameInfo();
 
-        Scanner inPuzzles = new Scanner(new File("puzzles.txt"));
-        String[] puzzles = new String[Integer.parseInt(inPuzzles.nextLine())];
-        for (int i = 0; i < puzzles.length; i++) {
-            puzzles[i] = inPuzzles.nextLine();
-        }
-        inPuzzles.close();
+        String[] allowed = readWords("allowed.txt");
+        String[] puzzles = readWords("puzzles.txt");
 
         Random r = new Random();
         int puzzleNumber = r.nextInt(puzzles.length);
@@ -50,21 +119,9 @@ public class Proj5 {
             String guess = s.nextLine().toLowerCase();
 
             //build spaced word
-            StringBuilder build = new StringBuilder();
-            for (int i = 0; i < guess.length(); i++) {
-                build.append(guess.charAt(i) + " ");
-            }
-            String spacedGuess = build.toString();
+            String spacedGuess = buildSpacedWord(guess);
 
-            boolean found = false;
-            for (int i = 0 ; i < allowed.length; i++) {
-                if (guess.equals(allowed[i])) found = true;
-            }
-            for (int i = 0 ; i < puzzles.length; i++) {
-                if (guess.equals(puzzles[i])) found = true;
-            }
-
-            if (!found) {
+            if (!isValid(guess, allowed, puzzles)) {
                 System.out.printf("%s is not a valid word%n%n", guess);
                 continue;
             }
@@ -74,40 +131,7 @@ public class Proj5 {
             //Y = correct letter, wrong spot
             //B = wrong letter 
 
-            char[] feedback = new char[guess.length()];
-            boolean[] used = new boolean[guess.length()];
-
-            //find the correct letter/correct spot (G), mark that spot as used
-            for (int i = 0; i < guess.length(); i++) {
-                if (guess.charAt(i) == correct.charAt(i)) {
-                    feedback[i] = 'G';
-                    used[i] = true;
-                }
-            }
-
-            //now go back and find the correct letters/wrong spots (Y)
-            //if guess has x occurrences of the letter and correct has y occurrences, and x > y, only
-            //the first y occurrences in guess should get marked as Y (or G)
-            for (int i = 0; i < guess.length(); i++) {
-                if (feedback[i] == 'G') continue;
-
-                boolean matched = false;
-                for (int j = 0; j < correct.length(); j++) {
-                    if (guess.charAt(i) == correct.charAt(j) && !used[j]) {
-                        //only possible if i != j
-
-                        matched = true;
-
-                        //prevents us from matching the same correct letter against a different (duplicate)
-                        //letter in guess
-                        used[j] = true;
-                        break;
-                    }
-                }
-
-                if (matched) feedback[i] = 'Y';
-                else feedback[i] = 'B';
-            }
+            char[] feedback = buildFeedback(guess, correct);
 
             System.out.println("Feedback: ");
             System.out.println("\t" + spacedGuess);
