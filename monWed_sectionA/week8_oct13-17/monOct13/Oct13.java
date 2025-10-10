6bcbdf5f2d925cc2d57be0b3674b379b4f4c9b71
Julie <juliet@ksu.edu>
2025-10-10 13:29:40 -0500

    notes before week 8

---
+++ monWed_sectionA/week8_oct13-17/monOct13/Oct13.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Oct13 {
+    public static void main(String[] args) {
+        //command-line args: word number
+
+        //print 1st letter of word
+        //print one more than number
+
+        //print error if one or both not provided
+    }
+}
