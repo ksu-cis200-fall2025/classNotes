67c425f050897fc7225b72ea130f800d07ce1739
Julie <juliet@ksu.edu>
2025-10-13 14:15:06 -0500

    afte rmon oct 13

--- monWed_sectionA/week8_oct13-17/monOct13/Oct13.java
+++ monWed_sectionA/week8_oct13-17/monOct13/Oct13.java
@@ -4,6 +4,16 @@ public class Oct13 {
     public static void main(String[] args) {
         //command-line args: word number
 
+        if (args.length == 2) {
+            String word = args[0];
+            System.out.println(word.charAt(0));
+            int val = Integer.parseInt(args[1]) + 1;
+            System.out.println(val);
+        }
+        else {
+            System.out.println("Error. Run as: java Oct13 word number");
+        }
+
         //print 1st letter of word
         //print one more than number
 
