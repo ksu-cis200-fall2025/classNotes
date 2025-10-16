c7f3ad2d96acd3c5783d982f4c4b8135722a97d7
Julie <juliet@ksu.edu>
2025-10-16 09:25:35 -0500

    added lab 13 sol

--- tuesThurs_sectionB/week8_oct13-17/tuesOct14/Oct14.java
+++ tuesThurs_sectionB/week8_oct13-17/tuesOct14/Oct14.java
@@ -2,7 +2,18 @@ import java.util.*;
 
 public class Oct14 {
     public static void main(String[] args) {
+        if (args.length == 2) {
+            char letter = args[0].charAt(0);
+            int num = Integer.parseInt(args[1]);
+
+            System.out.println(letter + " " + (num+1));
+        }
+        else {
+            System.out.println("Error. Run as: java Oct14 word number");
+        }
+
         //command-line args: word number
+        //java Oct14 apple 15
 
         //print 1st letter of word
         //print one more than number
