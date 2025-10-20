778199824d53daa9168b8119c09437c54cc11a2d
Julie <juliet@ksu.edu>
2025-10-20 14:03:22 -0500

    after mon oct 20

--- monWed_sectionA/week9_oct20-24/monOct20/Fraction.java
+++ monWed_sectionA/week9_oct20-24/monOct20/Fraction.java
@@ -1,3 +1,32 @@
 public class Fraction {
-    
+    //fields
+    private int num;
+    private int denom;
+
+    //constructor
+    public Fraction(int n, int d) {
+        num = n;
+        denom = d;
+    }   
+
+    public void print() {
+        System.out.printf("%d/%d%n", num, denom);
+    }
+
+    //addition
+    public Fraction plus(Fraction other) {
+        //num and denom are the info for the calling object
+        //(THIS fraction)
+
+        //need new numerator
+        int newNum = num*other.denom + other.num*denom;    
+
+        //need new denominator
+        //multiply the two denominators
+        int newDenom = denom * other.denom;
+
+        Fraction answer = new Fraction(newNum, newDenom);
+
+        return answer;
+    }
 }
--- monWed_sectionA/week9_oct20-24/monOct20/Oct20.java
+++ monWed_sectionA/week9_oct20-24/monOct20/Oct20.java
@@ -4,6 +4,23 @@ public class Oct20 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        
+        //want to represent a circle with radius 4.5
+        Circle c = new Circle(4.5);
+        System.out.println(c.area());
+
+        //another circle, radius 2
+        Circle c2 = new Circle(2);
+        System.out.println(c2.area());
+
+        c2 = c;
+
+        //fraction 3/4
+        Fraction f1 = new Fraction(3, 4);
+
+        //fraction 2/3
+        Fraction f2 = new Fraction(2, 3);
+
+        //add them together
+        Fraction result = f1.plus(f2);
     }
 }
