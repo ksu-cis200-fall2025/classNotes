77d09127ee2dba891983eb9f6c1d98ec8c777e29
Julie <juliet@ksu.edu>
2025-10-22 11:36:16 -0500

    added start for oct 22 and 23

---
+++ tuesThurs_sectionB/week9_oct20-24/thursOct23/Circle.java
@@ -0,0 +1,19 @@
+public class Circle {
+    //fields
+    private double radius;
+
+    //constructor
+    public Circle(double r) {
+        radius = r;
+    }
+
+    //operations (circumference, area)
+
+    public double circumference() {
+        return 2*3.14*radius;
+    }
+
+    public double area() {
+        return 3.14*radius*radius;
+    }
+}
