652fd89ecd7a1e0d58f2edfe2197ccb1c009a7bd
Julie <juliet@ksu.edu>
2025-11-07 15:17:13 -0600

    notes before mon nov 10

---
+++ monWed_sectionA/week12_nov10-14/monNov10/Nov10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Nov10 {
+    public static void main(String[] args) {
+        //ArrayList example: collection of circles
+
+        //add a few
+
+        //then print those with area between 5-10
+    }
+}
