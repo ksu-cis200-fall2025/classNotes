02998cb03e0c0955e48515fff2af4342a1a81aed
Julie <juliet@ksu.edu>
2025-11-10 13:53:44 -0600

    after mon nov 10

--- monWed_sectionA/week12_nov10-14/monNov10/Nameable.java
+++ monWed_sectionA/week12_nov10-14/monNov10/Nameable.java
@@ -1,4 +1,8 @@
 //make "Nameable" Interface
 
+public interface Nameable {
+    String name();
+}
+
 
 //a way to get a name
--- monWed_sectionA/week12_nov10-14/monNov10/Nov10.java
+++ monWed_sectionA/week12_nov10-14/monNov10/Nov10.java
@@ -1,11 +1,44 @@
 import java.util.*;
+import javax.swing.*;
 
 public class Nov10 {
     public static void main(String[] args) {
         //ArrayList example: collection of circles
 
+        /*
+        ArrayList<Circle> list = new ArrayList<Circle>();
+
         //add a few
+        Circle c = new Circle(3.5);
+        list.add(c);
+        list.add(new Circle(7.5));
 
         //then print those with area between 5-10
+        for (int i = 0; i < list.size(); i++) {
+            Circle cur = list.get(i);
+            if (cur.area() >= 5 && cur.area() <= 10) {
+                System.out.println(cur);
+            }
+        }
+
+        for (Circle cur : list) {
+            if (cur.area() >= 5 && cur.area() <= 10) {
+                System.out.println(cur);
+            }
+        }*/
+
+       /*
+       Nameable n = new Circle();
+
+       System.out.println(n.name());
+       */
+
+        //message box:
+        JOptionPane.showMessageDialog(null, "Hello, world!");
+
+        //input box:
+        String name = JOptionPane.showInputDialog("Enter your name: ");
+
+        JOptionPane.showMessageDialog(null, "Hello " + name);
     }
 }
