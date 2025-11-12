fe46de6ed551c5d74395a298bb8e0851c57fa512
Julie <juliet@ksu.edu>
2025-11-12 13:56:50 -0600

    after mon nov 12

--- monWed_sectionA/week12_nov10-14/wedNov12/Nov12.java
+++ monWed_sectionA/week12_nov10-14/wedNov12/Nov12.java
@@ -3,5 +3,18 @@ import java.util.*;
 public class Nov12 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
+        //create a TreeSet of several people
+        TreeSet<Person> set = new TreeSet<Person>();
+        set.add(new Person("Bob", 20));
+        set.add(new Person("Jill", 25));
+        set.add(new Person("Bob", 22));
+        set.add(new Person("Bob", 20));
+
+
+        //loop to print all information
+        for (Person p : set) {
+            System.out.println(p);
+        }
     }
 }
--- monWed_sectionA/week12_nov10-14/wedNov12/Person.java
+++ monWed_sectionA/week12_nov10-14/wedNov12/Person.java
@@ -1,4 +1,4 @@
-public class Person {
+public class Person implements Comparable<Person> {
     private String name;
     private int age;
 
@@ -9,5 +9,18 @@ public class Person {
 
     public String toString() {
         return String.format("Name: %s, age: %d%n", name, age);
+    }
+
+    public int compareTo(Person p) {
+        if (name.compareTo(p.name) < 0) return -1;
+        else if (name.compareTo(p.name) == 0) {
+            if (age < p.age) return -1;
+            else if (age == p.age) return 0;
+            else return 1;
+        }
+        else {
+            //name.compareTo(p.name) > 0
+            return 1;
+        }
     }
 }
