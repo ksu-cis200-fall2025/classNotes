6b64f43a9b752020ce2dfc3fc564123782a80a68
Julie <juliet@ksu.edu>
2025-12-03 13:58:33 -0600

    notes after wed dec 3

--- monWed_sectionA/week14_dec1-5/wedDec3/examples/Rectangle.cs
+++ monWed_sectionA/week14_dec1-5/wedDec3/examples/Rectangle.cs
@@ -1,6 +1,30 @@
 public class Rectangle {
-    private int _length;
-    private int _width;
+    private int _length = 1;
+    private int _width = 1;
+
+    public int Length {
+        //both get and set access
+        get {
+            return _length;
+        }
+        set {
+            if (value >= 1) {
+                _length = value;
+            }
+        }
+    }
+
+    public int Width {
+        //both get and set access
+        get {
+            return _width;
+        }
+        set {
+            if (value >= 1) {
+                _width = value;
+            }
+        }
+    }
 
     //make Length, Width properties
     //default Length/Width are each 1
@@ -9,13 +33,18 @@ public class Rectangle {
     //make Area a property instead too
 
     //override ToString
-
-    public Rectangle(int l, int w) {
-        _length = l;
-        _width = w;
+    public override string ToString() {
+        return $"{Length}x{Width} rectangle, area {Area}";
     }
 
-    public int Area() {
-        return _length*_width;
+
+    //add Name property ("Rectangle")
+    public string Name => "Rectangle";
+
+
+    public int Area {
+        get {
+            return _length*_width;
+        }
     }
 }
