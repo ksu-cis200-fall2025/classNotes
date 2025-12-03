5b3aa25931a3c63ce24bc9013ecfe3495b910b13
Julie <juliet@ksu.edu>
2025-12-03 11:38:22 -0600

    notes before dec 3 and 4

---
+++ tuesThurs_sectionB/week14_dec1-5/thursDec4/examples/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+//create a list of rectangles
+
+//read from input file
+
+//print all information
+
+List<Rectangle> list = new List<Rectangle>();
+
+try {
+    using (StreamReader reader = new StreamReader("dim.txt")) {
+        while (!reader.EndOfStream) {
+            string line = reader.ReadLine();
+
+            string[] pieces = line.Split(' ');
+
+            //how to add rectangle with new info to the list?
+        }
+    }
+}
+catch (IOException) {
+    Console.WriteLine("error reading file");
+}
+
+//how to print all rectangles?
